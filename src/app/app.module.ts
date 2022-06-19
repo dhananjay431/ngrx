f756d3824e1c91f10388ca536b640a3f89e90e52
dhananjay431 <dbel431@gmail.com>
2022-06-19 15:52:15 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { StoreModule } from '@ngrx/store';
+
 @NgModule({
   imports: [
     BrowserModule,
