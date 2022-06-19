8e0eadbe17c5811ceaff32fe6d95a7431bd6a8de
dhananjay431 <dbel431@gmail.com>
2022-06-19 15:44:15 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-  aj(){
-    
+export class AppComponent implements OnInit {
+  name :any;
+  oa = (d) => d;
+  aj(url) {
+   let d2 = fetch(url).then(response => response.json());
+    return from(d2);
+  }
+  ngOnInit() {
+    this.name = this.aj('https://jsonplaceholder.typicode.com/users');
   }
 }
