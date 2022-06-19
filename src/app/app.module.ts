14aeb7d25e8320f0890ae58c60ace9965fcef3bd
dhananjay431 <dbel431@gmail.com>
2022-06-19 17:03:34 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,22 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 // import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+//import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     // StoreModule.forRoot({}),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,
-      logOnly: false,
-      autoPause: true,
-      features: {
-        pause: false,
-        lock: true,
-        persist: true,
-      },
-    }),
+    // StoreDevtoolsModule.instrument({
+    //   maxAge: 25,
+    //   logOnly: false,
+    //   autoPause: true,
+    //   features: {
+    //     pause: false,
+    //     lock: true,
+    //     persist: true,
+    //   },
+    // }),
   ],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
